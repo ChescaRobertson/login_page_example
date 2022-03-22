32f775f619ad04ec7cf9f66e7f1271c294ff3f19
ChescaRobertson <francescamarierobertson@hotmail.co.uk>
2022-03-22 10:58:14 +0000

    Refactored to remove validate username using try and catch

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ app.set('views', 'views'); // Path to views folder
 app.set('view engine', 'ejs'); // Declaring that you are using ejs view engine
 
 app.get('/', (req, res) => {
-  const name = 'Francesca';
-  res.render('index', { name: name });
-});
-
-app.get('/testdb', (req, res) => {
-  getAllData(req, res);
+  res.render('index');
 });
 
 app.get('/login', (req, res) => {
@@ -27,7 +22,7 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res, next) => {
-  validateUsername(req.body, res);
+  processLogin(req.body, res);
 });
 
 app.get('/register', (req, res) => {
@@ -47,7 +42,6 @@ function createUser(params, res) {
   let tableName = 'testtable';
   let columnName = 'f_name, l_name, id, username, password';
   let myQuery = `INSERT INTO "${tableName}" (${columnName}) VALUES ('${f_name}', '${l_name}', DEFAULT, '${username}', '${password}')`;
-  console.log(myQuery);
   client.query(myQuery, (err, result) => {
     if (err) {
       res.render('register', { message: 'Username already exists' });
@@ -57,50 +51,22 @@ function createUser(params, res) {
   });
 }
 
-function validateUsername(params, res) {
-  let username = params.username;
-  let userexists = false;
-
-  let tableName = 'testtable';
-  let columnName = 'username';
-  let myQuery = `SELECT ${columnName} FROM "${tableName}"`;
-  console.log(myQuery);
-  client.query(myQuery, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      for (let i = 0; i < result.rows.length; i++) {
-        if (result.rows[i].username === username) {
-          userexists = true;
-          validatePassword(params, res);
-          break;
-        }
-      }
-      if (!userexists) {
-        res.render('login', { message: 'Invalid username or password' });
-      }
-    }
-  });
-}
-
-function validatePassword(params, res) {
+function processLogin(params, res) {
   let password = params.password;
   let username = params.username;
 
   let tableName = 'testtable';
   let columnName = 'username';
   let myQuery = `SELECT password FROM "${tableName}" WHERE ${columnName} = '${username}'`;
-  console.log(myQuery);
   client.query(myQuery, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let dbpass = result.rows[0].password;
-      if (dbpass === password) {
+    try {
+      if (result.rows[0].password === password) {
         getUserData(username, res);
       } else {
         res.render('login', { message: 'Invalid username or password' });
       }
+    } catch {
+      res.render('login', { message: 'Invalid username or password' });
     }
   });
 }
@@ -112,7 +78,6 @@ function getUserData(username, res) {
   client.query(myQuery, (err, result) => {
     if (err) {
       console.log(err);
-      res.status(500).send(err);
     } else {
       let data = result.rows;
       res.render('welcome', { data: data });
@@ -120,29 +85,6 @@ function getUserData(username, res) {
   });
 }
 
-function getAllData(req, res) {
-  let tableName = 'testtable';
-  let myQuery = `SELECT * FROM "${tableName}"`;
-  client.query(myQuery, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send(err);
-    } else {
-      let data = result.rows;
-      res.render('testdb', { data: data });
-    }
-  });
-}
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-// Challenge:
-// On logon page
-// Create paragraph with href to click here to register
-// If click: register account page (form, post username, password), post into same form
-// Create function
-// Insert into table values
-// If successful redirect to login page with message
-// If unsuccesful error messag
