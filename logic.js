46aca9a85dd8a0c37d4ec990213bde820848d5c1
ChescaRobertson <francescamarierobertson@hotmail.co.uk>
2022-03-30 09:25:58 +0100

    Tidy up

--- logic.js
+++
@@ -1,7 +0,0 @@
-let isAdmin = document.getElementById('isAdmin').value;
-
-if (isAdmin === 'Y') {
-  document.getElementById('admintoggle').style.display = 'block';
-} else {
-  document.getElementById('admintoggle').style.display = 'none';
-}
